49b1663845de86df3d5cf65558621a958a019dc8
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-09 21:44:07 +0100

    Add pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <Component {...pageProps} key={router.asPath} />
+    </AnimatePresence>
+  );
+}
+
+export default MyApp;
